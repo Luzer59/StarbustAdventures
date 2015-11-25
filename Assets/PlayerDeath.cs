8da63c21ab648b25d12c2473ac7b3d04d35fb30a
Luzer59 <luzer59@hotmail.com>
2015-11-25 15:23:12 +0200

    Gameover tai jotain...

---
+++ Assets/PlayerDeath.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerDeath : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tex;
+
+    public float fadeSpeed = 1f;
+
+    public IEnumerator EndGame()
+    {
+        Image image = tex.GetComponent<Image>();
+        /*yield return new WaitForSeconds(1f);
+        for (float i = 0f; i <= 255f; i += fadeSpeed * Time.deltaTime)
+        {
+            print(image.color.a);
+            image.color = new Color(255f,255f,255f,i);
+            yield return null;
+        }
+        yield return new WaitForSeconds(3f);
+        Application.LoadLevel(Application.loadedLevel);*/
+    }
+}
