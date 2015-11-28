9bb0590e10301df923e8112f28d4dbe426065734
Luzer59 <luzer59@hotmail.com>
2015-11-28 13:16:27 +0200

    Score display and new player hitbox

---
+++ Assets/DisplayScore.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayScore : MonoBehaviour
+{
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("Score"))
+        {
+            PlayerPrefs.SetInt("Score", 0);
+        }
+        GetComponent<Text>().text = PlayerPrefs.GetInt("Score").ToString();
+    }
+}
