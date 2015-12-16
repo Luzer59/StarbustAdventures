71c11a23f4b4c4aef009c9bcb8e95b41656cce56
Luzer59 <luzer59@hotmail.com>
2015-12-16 17:58:00 +0200

    Intro fixattu

---
+++ Assets/EndTimer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndTimer : MonoBehaviour
+{
+    public float time;
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0f)
+        {
+            GetComponent<MenuController>().ChangeLevel(0);
+        }
+    }
+}
