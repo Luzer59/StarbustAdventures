b1585581acc682589c6945dc11549659c092a622
Luzer59 <luzer59@hotmail.com>
2015-12-16 14:06:07 +0200

    Menu BGB toimi(nee)

--- Assets/MenuAudioController.cs
+++ Assets/MenuAudioController.cs
@@ -3,14 +3,24 @@ using System.Collections;
 
 public class MenuAudioController : MonoBehaviour
 {
-    void Awake()
+
+    void OnLevelWasLoaded(int index)
     {
-        if (GameObject.Find("AudioController"))
+        if (index == 2 || index == 4)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        if (GameObject.Find("ActiveAudioController"))
         {
             Destroy(gameObject);
         }
         else
         {
+            name = "Active" + name;
             GetComponent<AudioSource>().Play();
             DontDestroyOnLoad(gameObject);
         }
