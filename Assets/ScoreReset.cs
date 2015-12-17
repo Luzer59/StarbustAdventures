ec8b27dd6532b795e026bc93e73626b85f2d10e3
Luzer59 <luzer59@hotmail.com>
2015-12-17 08:09:16 +0200

    Reset, kun painaa main menussa R-painiketta yli 3 sec

---
+++ Assets/ScoreReset.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreReset : MonoBehaviour
+{
+    private float timer = 0f;
+
+	void Update ()
+    {
+	    if (Input.GetKey(KeyCode.R))
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            timer = 0f;
+        }
+        if (timer >= 3f)
+        {
+            print("Score reset");
+            PlayerPrefs.SetInt("Level_" + 1 + "_score", 0);
+            PlayerPrefs.SetInt("Level_" + 2 + "_score", 0);
+        }
+	}
+}
